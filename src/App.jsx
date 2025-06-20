55eb55016fa865c8443b640cc6ac21e17fe1c2d8
Hatsunmikk <debos2003@gmail.com>
2025-06-20 19:41:33 +0530

    Initialize project with Vite, React and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100 text-purple-700">
+      <h1 className="text-4xl font-bold underline">Tailwind is Working!</h1>
+      <p className="mt-4 text-lg">This is a test page.</p>
+    </div>
+  )
+}
+
+export default App;
